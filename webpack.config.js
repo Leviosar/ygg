467d6316f2259891d6a641b90c31ad516a4fd869
Joao Vitor Maia <maia.tostring@gmail.com>
2022-05-22 01:35:22 -0300

    Modifica targets de compilação do tsc e webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: {
+        'ygg': './src/index.ts',
+        'ygg.min': './src/index.ts'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'ygg',
+        umdNamedDefine: true
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
     devtool: 'inline-source-map',
+    module: {
+        rules: [
+            { test: /\.ts/, use: 'ts-loader' }
+        ]
+    }
 };
